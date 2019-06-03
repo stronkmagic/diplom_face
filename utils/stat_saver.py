dda3be5fe33ef1a3e76f01bd09b193d16ef5b1e6
Vladlen Kyselov <vladlenkiselyov@gmail.com>
2019-06-03 23:06:21 +0200

    added stat save file

---
+++ utils/stat_saver.py
@@ -0,0 +1,27 @@
+import os
+import csv
+
+# def save_metric(value, model_name, metric_name):
+#     filename = metric_name + '.csv'
+#     create_stat_dir_if_not_exists()
+#     with open(, 'r') as readFile:
+#         reader = csv.reader(readFile)
+#     lines = list(reader)
+#     lines[2] = row
+#
+#     with open('people.csv', 'w') as writeFile:
+#         writer = csv.writer(writeFile)
+#         writer.writerows(lines)
+#
+#     readFile.close()
+#     writeFile.close()
+#
+# def create_new_file_if_not_exists():
+#
+#
+#
+# def create_stat_dir_if_not_exists():
+#     directory = 'stats'
+#     if not os.path.exists(directory):
+#         os.makedirs(directory)
+#
